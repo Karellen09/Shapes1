54e771e96c91ca6370f32bf9cfacbcd744a9480f
Karellen09 <alexandru.both@gmail.com>
2016-03-19 14:53:27 +0000

    Initial commit

---
+++ 18.03.2016 - Drawing objects/Shapes1/Shapes1/Canvas1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Shapes1
+{
+   class helper
+    {
+        // variables:
+        //plate
+        Point origin = new Point(100, 100);
+        static double width = 100;
+        static double height = 600;
+        Size size = new Size(width, height);
+        //bolts
+        int diameter = 20;
+        Point bolt_origin = new Point(100, 100);
+
+
+
+
+        void draw_plate(DrawingContext dc, Point origin, Size size)
+        {
+            dc.DrawRectangle(Brushes.Gray,
+                new Pen(Brushes.Black, 2),
+                new Rect(origin, size));
+
+        }
+
+        void draw_bolt(DrawingContext dc, Point bolt_origin, double diameter)
+        {
+            dc.DrawEllipse(Brushes.White,
+                new Pen(Brushes.Black, 1),
+                bolt_origin,
+                diameter,
+                diameter);
+
+
+        }
+    }
+    
+}
---
+++ 18.03.2016 - Drawing objects/Shapes1/Shapes1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shapes1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyCanvas my_canvas = new MyCanvas();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IMG_Display.Children.Add(my_canvas);
+           
+        }
+
+
+
+    class MyCanvas : Canvas
+    {
+        public MyCanvas() : base()
+        {
+
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+                helper.draw_plate(dc,origin, size);
+                helper.draw_bolt(dc, bolt_origin,diameter);
+        }
+
+
+      
+        }
+    }
+}
